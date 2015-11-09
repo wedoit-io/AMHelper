3445d928347423e20b459843f1d2e85c319d6a6e
Stefano Teodorani <teopost@gmail.com>
2015-11-09 14:31:15 +0100

    Aggiunti campi per gestione UM preferenziale di vendita
    - Aggiunto campo in tracciato io_art_um.dat per definire quella predefinita
    - Aggiunto campo su io_art.dat per indicare se la UM suddetta è modificabile
    - Aggiunti commenti su campi UM e ART

--- src/net20/AMHelper/CSV/imp/rec_art.cs
+++ src/net20/AMHelper/CSV/imp/rec_art.cs
@@ -17,36 +17,138 @@ namespace AMHelper.CSV
     public class rec_art
     {
         // CHIAVE|COD_DITTA|COD_ART|DES_ART|COD_FAM|DES_FAM|COD_SFAM|DES_SFAM|COD_GRUPPO1|DES_GRUPPO1|COD_GRUPPO2|DES_GRUPPO2|UM1|UM2|FATTORE_CONVERSIONE|DES_GR_STAT1|DES_GR_STAT2|QTA_MIN_VEND|COD_CLASSE_SCONTO|COD_DEPERIBILITA|PREZZO_MIN_VEN|SCONTO_MAX_VEN|MAX_EXTRA_SCONTO|DAT_ULT_MOD
+        /// <summary>
+        /// Chiave univoca del record ereditata dai dati di origine
+        /// </summary>
         public string CHIAVE { get; set; }
+        /// <summary>
+        /// Codice della ditta. E' il codice che identifica univocamente una ditta dentro il gestionale che, nel caso sia multisocietario, può contenere dati di più aziende
+        /// </summary>
         public string COD_DITTA { get; set; }
+        /// <summary>
+        /// Codice dell'articolo
+        /// </summary>
         public string COD_ART { get; set; }
+        /// <summary>
+        /// Descrizione dell'articolo
+        /// </summary>
         public string DES_ART { get; set; }
+        /// <summary>
+        /// Codice della famiglia degli articoli
+        /// </summary>
         public string COD_FAM { get; set; }
+        /// <summary>
+        /// Descrizione della famiglia degli articoli
+        /// </summary>
         public string DES_FAM { get; set; }
+        /// <summary>
+        /// Codice della Famiglia
+        /// </summary>
         public string COD_SFAM { get; set; }
+        /// <summary>
+        /// Descrizione della sottofamiglia
+        /// </summary>
         public string DES_SFAM { get; set; }
+        /// <summary>
+        /// Codice del gruppo merceologico 1
+        /// </summary>
         public string COD_GRUPPO1 { get; set; }
+        /// <summary>
+        /// Descrizione del gruppo merceologico 1
+        /// </summary>
         public string DES_GRUPPO1 { get; set; }
+        /// <summary>
+        /// Codice del gruppo merceologico 2
+        /// </summary>
         public string COD_GRUPPO2 { get; set; }
+        /// <summary>
+        /// Descrizione del gruppo merceologico 2
+        /// </summary>
         public string DES_GRUPPO2 { get; set; }
+        /// <summary>
+        ///  Unità di misura 1
+        /// </summary>
         public string UM1 { get; set; }
+        /// <summary> 
+        ///  Unità di misura 2 (non usato)
+        /// </summary>
         public string UM2 { get; set; }
+        /// <summary>
+        /// Codice dello stato (non usato)
+        /// </summary>
         public string COD_STATO { get; set; }
+        /// <summary>
+        ///  Descrizione dello stato
+        /// </summary>
         public string DES_STATO { get; set; }
+        /// <summary>
+        /// Fattore di conversione (non usato)
+        /// </summary>
         public string FATTORE_CONVERSIONE { get; set; }
+        /// <summary>
+        /// Descrizione del gruppo statistico 1
+        /// </summary>
         public string DES_GR_STAT1 { get; set; }
+        /// <summary>
+        /// Descrizione del gruppo statistico 2
+        /// </summary>
         public string DES_GR_STAT2 { get; set; }
+        /// <summary>
+        /// Quantità minima di vendita.
+        /// Se impostata determina una quantità minima sotto la quale non è possibile ordinare merce
+        /// </summary>
         public string QTA_MIN_VEND { get; set; }
+        /// <summary>
+        /// Codice della classe sconto articolo
+        /// </summary>
         public string COD_CLASSE_SCONTO { get; set; }
+        /// <summary>
+        /// Codice deperibilità.
+        /// </summary>
         public string COD_DEPERIBILITA { get; set; }
+        /// <summary>
+        /// Indica un prezzo minimo di vendita sotto il quale non è possibile ordinare l'articolo
+        /// </summary>
         public string PREZZO_MIN_VEN { get; set; }
+        /// <summary>
+        /// Sconto massimo di vendita. Indica uno sconto massimo applicabile per l'articolo
+        /// </summary>
         public string SCONTO_MAX_VEN { get; set; }
+        /// <summary>
+        /// Sconot Massimo Extrasconto (non usato)
+        /// </summary>
         public string MAX_EXTRA_SCONTO { get; set; }
+        /// <summary>
+        /// Prezzo retail
+        /// </summary>
         public string PREZZO_RETAIL { get; set; }
+        /// <summary>
+        /// Descrizione del filtro 1. 
+        /// Valore descrittivo i cui valori distinti sono usati nell'iPad per configurare un filtro di ricerca
+        /// </summary>
         public string DES_FILTRO1 { get; set; }
+        /// <summary>
+        /// Descrizione del filtro 2 
+        /// Valore descrittivo i cui valori distinti sono usati nell'iPad per configurare un filtro di ricerca. Dipende da Filtro 1
+        /// </summary>
         public string DES_FILTRO2 { get; set; }
+        /// <summary>
+        /// Descrizione del filtro 3
+        /// Valore descrittivo i cui valori distinti sono usati nell'iPad per configurare un filtro di ricerca. Dipende da Filtro 2
+        /// </summary>
         public string DES_FILTRO3 { get; set; }
+        /// <summary>
+        /// Codice dello sviluppo. Usato solo per il modulo wTrendy
+        /// </summary>
         public string COD_SVILUPPO_BASE { get; set; }
+        /// <summary>
+        /// Flag che identifica la possibilità di modificare l'unità di misura di default in fase di inserimento ordine
+        /// Valori consentiti: 0 = Modificabile (default), -1 = Non modificabile
+        /// </summary>
+        public string FLG_MOD_UM_DEFAULT { get; set; }
+        /// <summary>
+        /// Data di ultima modifica del record
+        /// </summary>
         public string DAT_ULT_MOD { get; set; }
 
         //public string Nome { get { return "nomefile.dat";  } }
--- src/net20/AMHelper/CSV/imp/rec_art_um.cs
+++ src/net20/AMHelper/CSV/imp/rec_art_um.cs
@@ -10,16 +10,56 @@ using System.Text;
 
 namespace AMHelper.CSV
 {
+    /// <summary>
+    /// Tracciato record delle unità di misura articolo.
+    /// Contiene la lista di tutte le unità di misura possibili per ogni singolo Codice Articolo (COD_ART).
+    /// </summary>
     public class rec_art_um
     {
-        // CHIAVE|COD_DITTA|COD_ART|UM|DESC_UM|FAT_CONV|TIPO_UM|DAT_ULT_MOD
-        public string CHIAVE { get; set; }
-        public string COD_DITTA { get; set; }
+        // CHIAVE|COD_DITTA|COD_ART|UM|DESC_UM|FAT_CONV|TIPO_UM|FLG_DEFAULT|DAT_ULT_MOD
+
+        /// <summary>
+        /// Chiave univoca della riga
+        /// </summary>
+        public string CHIAVE { get; set; }    
+        /// <summary>
+        /// Codice della ditta. 
+        /// Identifica univocamente una ditta dentro il gestionale che, nel caso sia multisocietario, può contenere dati di più aziende
+        /// </summary>
+        public string COD_DITTA { get; set; }  
+        /// <summary>
+        /// Codice dell'articolo
+        /// </summary>
         public string COD_ART { get; set; }
+        // Codice dell'unità di misura (Es: PZ=Pezzi, KG=Chilogrammi, ecc...)
         public string UM { get; set; }
+        /// <summary>
+        /// Descrizione dell'unità di misura (Es: Pezzi, Chilogrammi, ecc...)
+        /// </summary>
         public string DESC_UM { get; set; }
+        /// <summary>
+        /// Fattore di conversione. 
+        /// Coefficiente da applicare alle unità di misura per ricondursi all'unità di misura principale (quella di tipo 1).
+        /// Per le unità di misura di tipo 1 non serve e va impostato a 0 (zero).
+        /// </summary>
         public string FAT_CONV { get; set; }
+        /// <summary>
+        /// Tipo Unità di misura.
+        /// Valori possibili:
+        ///   1 = Unità di misura principale (Può esserci solo una riga con FAT_CONV=0)
+        ///   2 = Unità di misura secondarie (Possono esserci più righe)
+        ///   3 = Confezioni. Viene trattato come UM di tipo 2 ma con possibilità di impostare controlli (es. Rottura confezioni)
+        /// </summary>
         public string TIPO_UM { get; set; }
+        /// <summary>
+        /// Flag che identifica l'unità di misura di default, ovvero quella che deve essere proposta quando si inserisce un nuovo ordine.
+        /// In molti gestionali è chiamata unità di misura di Vendita. 
+        /// Valori consentiti: 0 = Normale (default), -1 = Predefinita
+        /// </summary>
+        public string FLG_DEFAULT { get; set; }
+        /// <summary>
+        /// Data di ultima modifica del record
+        /// </summary>
         public string DAT_ULT_MOD { get; set; }
 
     }
