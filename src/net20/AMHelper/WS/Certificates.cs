12e43fc9b845a54c8fd29c7a5bb41a1ae50e552b
Stefano Teodorani <teopost@gmail.com>
2016-12-20 14:15:46 +0100

    Aggiunto metodo per consentire chiamate a siti https con certificati self-signed

--- src/net20/AMHelper/WS/Certificates.cs
+++ src/net20/AMHelper/WS/Certificates.cs
@@ -1,15 +1,25 @@
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-class Certificates
+public class Certificates
 {
-
+    /// <summary>
+    /// Ignora eventuali errori causati dall'utilizzo di certificati non validi
+    /// </summary>
     public static void IgnoreBadCertificates()
     {
         System.Net.ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
     }
 
-    public static bool AcceptAllCertifications(object sender,
+    /// <summary>
+    /// Ritorna sempre true per ignorare errori causati da ceritficati non validi
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="certificate"></param>
+    /// <param name="chain"></param>
+    /// <param name="policyErrors"></param>
+    /// <returns></returns>
+    static bool AcceptAllCertifications(object sender,
                                                 X509Certificate certificate,
                                                 X509Chain chain,
                                                 SslPolicyErrors policyErrors)
