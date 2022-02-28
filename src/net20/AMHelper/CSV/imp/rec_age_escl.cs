6b3714321ff6d6ba46a65cf83f79e2a9e9fa7cf4
r.pezzolati@wedoit.io <r.pezzolati@wedoit.io>
2022-02-28 14:15:23 +0100

    Gestione tracciati esclusione

---
+++ src/net20/AMHelper/CSV/imp/rec_age_escl.cs
@@ -0,0 +1,24 @@
+
+/*
+  Classe autogenerata dal tool RecClassCreator
+  by Stefano teodorani il 09/08/2013 14.35.39
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMHelper.CSV
+{
+    public class rec_age_escl
+    {
+        public string CHIAVE { get; set; }
+        public string COD_DITTA { get; set; }
+        public string COD_AGENTE { get; set; }
+        public string COD_FAM { get; set; }
+        public string COD_SFAM { get; set; }
+        public string DAT_ULT_MOD { get; set; }
+    }
+
+    
+}
---
+++ src/net20/AMHelper/CSV/imp/rec_cli_escl.cs
@@ -0,0 +1,25 @@
+
+/*
+  Classe autogenerata dal tool RecClassCreator
+  by Stefano teodorani il 09/08/2013 14.35.39
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMHelper.CSV
+{
+    public class rec_cli_escl
+    {
+        public string CHIAVE { get; set; }
+        public string COD_DITTA { get; set; }
+        public string TIPO_CLIFOR { get; set; }
+        public string COD_CLIFOR { get; set; }
+        public string COD_FAM { get; set; }
+        public string COD_SFAM { get; set; }
+        public string DAT_ULT_MOD { get; set; }
+    }
+
+    
+}
