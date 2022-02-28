5591932edf8cd84412f0113ddab1c7b9532ff3c9
r.pezzolati@wedoit.io <r.pezzolati@wedoit.io>
2022-02-28 14:37:43 +0100

    Aggiornamento tracciati

--- src/net20/AMHelper/CSV/imp/rec_age_escl.cs
+++ src/net20/AMHelper/CSV/imp/rec_age_escl.cs
@@ -10,13 +10,34 @@ using System.Text;
 
 namespace AMHelper.CSV
 {
+    /// <summary>
+    /// Classe Marche/Linee da escludere per gli agenti
+    /// </summary>
     public class rec_age_escl
     {
+        /// <summary>
+        /// Chiave
+        /// </summary>
         public string CHIAVE { get; set; }
+        /// <summary>
+        /// Codice Ditta
+        /// </summary>
         public string COD_DITTA { get; set; }
+        /// <summary>
+        /// Codice Agente
+        /// </summary>
         public string COD_AGENTE { get; set; }
+        /// <summary>
+        ///  Codice Marca
+        /// </summary>
         public string COD_FAM { get; set; }
+        /// <summary>
+        /// Codice Linea
+        /// </summary>
         public string COD_SFAM { get; set; }
+        /// <summary>
+        /// Data ultima modifica
+        /// </summary>
         public string DAT_ULT_MOD { get; set; }
     }
 
--- src/net20/AMHelper/CSV/imp/rec_cli_escl.cs
+++ src/net20/AMHelper/CSV/imp/rec_cli_escl.cs
@@ -10,14 +10,38 @@ using System.Text;
 
 namespace AMHelper.CSV
 {
+    /// <summary>
+    /// Classe Marche/Linee da escludere per i clienti
+    /// </summary>
     public class rec_cli_escl
     {
+        /// <summary>
+        /// Chiave
+        /// </summary>
         public string CHIAVE { get; set; }
+        /// <summary>
+        /// Codice Ditta
+        /// </summary>
         public string COD_DITTA { get; set; }
+        /// <summary>
+        /// Tipologia C/F
+        /// </summary>
         public string TIPO_CLIFOR { get; set; }
+        /// <summary>
+        /// Codice Cliente
+        /// </summary>
         public string COD_CLIFOR { get; set; }
+        /// <summary>
+        /// Marca
+        /// </summary>
         public string COD_FAM { get; set; }
+        /// <summary>
+        /// Linea
+        /// </summary>
         public string COD_SFAM { get; set; }
+        /// <summary>
+        /// Data Ultima modifica
+        /// </summary>
         public string DAT_ULT_MOD { get; set; }
     }
 
