ea302d1ed6f45be9baa92ce89549c994b9b057da
r.pezzolati@wedoit.io <r.pezzolati@wedoit.io>
2022-04-13 17:59:16 +0200

    Aggiunta tracciati magazzini

---
+++ src/net20/AMHelper/CSV/imp/rec_magazzini.cs
@@ -0,0 +1,23 @@
+
+/*
+  Classe autogenerata dal tool RecClassCreator
+  by Stefano teodorani il 09/08/2013 14.35.39
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMHelper.CSV
+{
+    public class rec_magazzini
+    {
+        public string CHIAVE { get; set; }
+        public string COD_DITTA { get; set; }
+        public string COD_MAGAZZINO { get; set; }
+        public string DES_MAGAZZINO { get; set; }
+        public string DAT_ULT_MOD { get; set; }
+    }
+
+
+}
